07f4ab60113321cbc05ab49f3e81b0d0f6e8419b
waterdragon <tndyd5390@naver.com>
2022-05-25 11:28:38 +0900

    chore: setting querydsl

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,40 @@
 package study.querydsl;
 
+import static org.assertj.core.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	// @PersistenceContext
+	@Autowired
+	EntityManager entityManager;
+
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		entityManager.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(entityManager);
+
+		QHello qHello = new QHello("h");
+
+		Hello result = query.selectFrom(qHello).fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
+	}
 
 }
