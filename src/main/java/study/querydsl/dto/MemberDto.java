e6e991905b9f4cc749720f91b6984614932944b8
waterdragon <tndyd5390@naver.com>
2022-06-05 15:08:19 +0900

    test: queryprojection

--- src/main/java/study/querydsl/dto/MemberDto.java
+++ src/main/java/study/querydsl/dto/MemberDto.java
@@ -1,5 +1,7 @@
 package study.querydsl.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
+
 import lombok.Data;
 
 @Data
@@ -10,6 +12,7 @@ public class MemberDto {
 	public MemberDto() {
 	}
 
+	@QueryProjection
 	public MemberDto(String username, int age) {
 		this.username = username;
 		this.age = age;
