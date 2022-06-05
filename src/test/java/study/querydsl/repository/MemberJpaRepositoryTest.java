7cee34bdc7b356b98365a346c8517f23d33b657f
waterdragon <tndyd5390@naver.com>
2022-06-05 17:29:08 +0900

    feat; dynamic query with jpa

--- src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -11,7 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 
 @SpringBootTest
 @Transactional
@@ -35,5 +38,34 @@ class MemberJpaRepositoryTest {
 
 		List<Member> result2 = memberJpaRepository.findByUsername_Querydsl("member1");
 		assertThat(result2).containsExactly(member);
+	}
+
+	@Test
+	public void searchTest() {
+		Team teamA = new Team("teamA");
+		Team teamB = new Team("teamB");
+
+		em.persist(teamA);
+		em.persist(teamB);
+
+		Member member1 = new Member("member1", 10, teamA);
+		Member member2 = new Member("member2", 20, teamA);
+		Member member3 = new Member("member3", 30, teamB);
+		Member member4 = new Member("member4", 40, teamB);
+		em.persist(member1);
+		em.persist(member2);
+		em.persist(member3);
+		em.persist(member4);
+
+		//동적 쿼리는 조건이 없으며 다 긁어 오기 때문에 기본조건, 리미트, 페이징이 있으면 좋다
+		MemberSearchCondition condition = new MemberSearchCondition();
+		condition.setAgeGoe(35);
+		condition.setAgeLoe(40);
+		condition.setTeamName("teamB");
+
+		List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+
+		assertThat(result).extracting("username").containsExactly("member3", "member4");
+
 	}
 }
